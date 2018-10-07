0f6e69015d98a288a8cbbf19c5f6cd9eded63d2a
Kartikey Tripathi <kartikey.tripathi.37@gmail.com>
2018-10-07 23:14:10 +0530

    add base colors

--- bounce.cpp
+++ bounce.cpp
@@ -1,1 +1,11 @@
-// Add Code Here
+#include <GLUT/glut.h>
+#include <cmath>
+#include <stdlib.h>
+
+// Colors
+GLfloat WHITE[] = {1, 1, 1};
+GLfloat RED[] = {1, 0, 0};
+GLfloat GREEN[] = {1, 0, 0};
+GLfloat MAGENTA[] = {1, 0, 1};
+
+//Add Camera Class
