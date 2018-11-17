e0d262f36ea0df683b476ef329a28d0c3c40c665
Kartikey Tripathi <kartikey.tripathi.37@gmail.com>
2018-11-17 20:28:53 +0530

    add class checkerboard

--- bounce.cpp
+++ bounce.cpp
@@ -54,4 +54,36 @@ public:
   }
 };
 
-
+// A checkerboard.  
+class Checkerboard {
+  int displayListId;
+  int width;
+  int depth;
+public:
+  Checkerboard(int width, int depth): width(width), depth(depth) {}
+  double centerx() {return width / 2;}
+  double centerz() {return depth / 2;}
+  void create() {
+    displayListId = glGenLists(1);
+    glNewList(displayListId, GL_COMPILE);
+    //GLfloat lightPosition[] = {4, 3, 7, 1};
+    //glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+    glBegin(GL_QUADS);
+    glNormal3d(0, 1, 0);
+    for (int x = 0; x < width - 1; x++) {
+      for (int z = 0; z < depth - 1; z++) {
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE,
+                     (x + z) % 2 == 0 ? RED : WHITE);
+        glVertex3d(x, 0, z);
+        glVertex3d(x+1, 0, z);
+        glVertex3d(x+1, 0, z+1);
+        glVertex3d(x, 0, z+1);
+      }
+    }
+    glEnd();
+    glEndList();
+  }
+  void draw() {
+    glCallList(displayListId);
+  }
+};
